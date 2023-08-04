567a49293175008bb55ca15c0099dd245e5c43c8
minsik321 <alstlr832110@gmail.com>
2023-08-04 14:12:05 +0900

    아이스크림 만들기

---
+++ IceCream.java
@@ -0,0 +1,14 @@
+public class IceCream {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 11; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("%5s%2s\n", "|","|");
+        }
+    }
+}
+    
